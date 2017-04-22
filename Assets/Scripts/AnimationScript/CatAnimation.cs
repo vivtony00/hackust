03cbdf285b56fe04d131471702bc2e04dc17a8a9
DESKTOP-C8HFLAI\Nicole&Chung <yyhoae@connect.ust.hk>
2017-04-23 03:48:32 +0800

    - add animation and character prefab

---
+++ Assets/Scripts/AnimationScript/CatAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
